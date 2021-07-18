fbf0479b0a19458f679af564f9bd66ff030e4265
atinching <atin5551@gmail.com>
2021-07-18 23:13:57 +0300

    Defined and implemented node class

--- Data Structures/Linked List/Doubly Linked List.cpp
+++ Data Structures/Linked List/Doubly Linked List.cpp
@@ -1,4 +1,20 @@
-template<class T>
+#include <string>
+template<typename T>
+class Node {  // Node defintion
+	private:
+		T data; // Data stored in the node
+		Node<T> next, prev; // Next and previous nodes of this node.
+	public:
+		Node(T data, Node<T> prev, Node<T> next) {
+			this->data = data;
+			this->prev = prev;
+			this->next = next;
+		}
+};
 class LinkedList {
+	public:
+		LinkedList() {}
+		void clear() {
 
+		}
 };
