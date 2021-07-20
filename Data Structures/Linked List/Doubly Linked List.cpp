6bebe446c7c6b0d69649a95ab24b15d189268079
atinching <atin5551@gmail.com>
2021-07-21 02:33:27 +0300

    Finished implementation of doubly linked list
    - Finished implementing doubly linked list. Implemented removeAt(), remove(), indexOf() and contains().
    - Not tested yet, so most likely has bugs.

--- Data Structures/Linked List/Doubly Linked List.cpp
+++ Data Structures/Linked List/Doubly Linked List.cpp
@@ -16,6 +16,16 @@ class LinkedList {
 		int size = 0;
 		Node<T> head = NULL;
 		Node<T> tail = NULL;
+		T remove(Node<T> node) {
+			if (node.next == NULL) return removeLast(); // Remove the tail if tail node was passed in.
+			if (node.prev == NULL) return removeFirst(); // Remove the head if head node was passed in.
+			node.next.prev = node.prev; // Setting the pointers of adjacent nodes to skip/point before and after the node passed in, respectively.   
+			node.prev.next = node.next;
+
+			T data = node.data; // Node data has to be returned after node removal.
+			node.data = NULL; // Deallocating/removing/erasing the memory/data of the node that has to be removed. 
+			return data;
+		}
 	public:
 		LinkedList() {}
 		void clear() {
@@ -86,4 +96,70 @@ class LinkedList {
 			else { tail.next = NULL; }
 			return data;
 		}
+		T removeAt(int index) {
+			if (index < 0 || index >= size) throw std::invalid_argument("Invalid index!");
+			int i = 0;
+			if (index < size / 2) { // Search from the front of the list
+				for (i = 0, trav = head; i != index; i++) // Start at head, then keep traversing.
+				{
+					trav = trav.next // Keep moving/iterating through the linked list, by using the traverser. 
+				}
+				return remove(trav); // We've now reached the node at the passed in index so we can just remove it.
+			}
+			else { // Search from the back of the list
+				for (i = size, trav = tail; i != index; i--) {
+					trav = trav.prev;
+				}
+				return remove(trav);
+			}
+		}
+		bool remove(T obj) { // Remove certain object from the linked list.
+			Node<T> trav = head;
+			if (obj == NULL) { // If object passed in is NULL type.
+				for (trav = head; trav != NULL; trav = trav.next) { // Start from head node, keep iterating through nodes unless the trav reaches the end of the list.
+					if (trav.data == NULL) { // If the current node's data holds NULL then we remove it since we've found it.
+						remove(trav);
+						return true;
+					}
+				}
+			}
+			else { // If object passed in is not null
+				for (trav = head; trav != NULL; trav = trav.next) {
+					if (trav.data == obj) {
+						remove(trav);
+						return true;
+					}
+				}
+			}
+			return false; // Return false if the object could not be found and removed.
+		}
+		int indexOf(T obj) { // Similar logic to method above
+			Node<T> trav = head;
+			int i = 0; // Used for storing index of current iteration
+			if (obj == NULL) {
+				for (trav = head; trav != NULL; trav = trav.next, i++) { // Keep iterating through nodes in the linked list and incrementing index
+					if (trav.data = NULL) {
+						return i; // Return current index if current nodes data is null
+					}
+				}
+			}
+			else {
+				for (trav = head; trav != NULL; trav = trav.next, i++) {
+					if (trav.data == obj) {
+						return i; // Return index of node that has same data/value as object passed in.
+					}
+				}
+			}
+			return -1; // Return -1 if object could not be found
+		}
+		bool contains(T obj) {
+			return indexOf(obj) == -1; // If index of object passed in is -1 then that means it doesn't exist in the linked list.
+		}
+		int main() {
+		
+		}
 };
+
+
+
+
