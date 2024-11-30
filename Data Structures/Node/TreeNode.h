a988ce592b67044d232f1d583a8f70a812b5818d
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2024-11-29 16:03:02 -0800

    Delete Data Structures/Node directory

--- Data Structures/Node/TreeNode.h
+++
@@ -1,6 +0,0 @@
-// Simple binary search tree node declaration that will be used a lot in the other graph theory algorithms
-typedef struct {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-} TreeNode;
