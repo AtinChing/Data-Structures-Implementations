3938dfbecb76f2f4d27265aed82c84efd965592a
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2024-11-29 16:02:20 -0800

    Added Binary Search Tree Node

---
+++ Data Structures/Node/TreeNode.h
@@ -0,0 +1,6 @@
+// Simple binary search tree node declaration that will be used a lot in the other graph theory algorithms
+typedef struct {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+} TreeNode;
