8be2d3045e33c6bccb839b34cbc6628686e1a6d2
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2024-12-24 03:36:10 -0500

    Added remove edge functionality
    - Might need to split a separate cpp file for weighted graph node

--- Data Structures/Nodes/WeightedGraphNode.h
+++ Data Structures/Nodes/WeightedGraphNode.h
@@ -3,13 +3,18 @@
 class GraphNode {
 public:
     int val;  // Value or identifier for the node
-    std::vector<pair<GraphNode*, int>> adj;  // List of adjacent nodes (neighbors)
+    std::vector<std::pair<GraphNode*, int>> adj;  // List of adjacent nodes (neighbors)
 
     // Constructor to initialize the node with a value
     GraphNode(int x) : val(x) {}
-
+    int getInd(GraphNode* neighbor, int weight){
+        for(int i = 0; i < adj.size(); i++){if(adj[i].first == neighbor && adj[i].second == weight) return i;}
+    }
     // Add an edge to an adjacent node
     void addEdge(GraphNode* neighbor, int weight) {
-        adj.push_back(make_pair(neighbor, weight));
+        adj.push_back(std::make_pair(neighbor, weight));
+    }
+    void removeEdge(GraphNode* neighbor, int weight){
+        adj.erase(adj.begin() + getInd(neighbor, weight));
     }
 };
